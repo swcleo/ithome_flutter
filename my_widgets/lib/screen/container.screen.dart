9b3d98f9b2c0e6c2d563ee2183008664cad4e5d7
leo_chen <leo_chen@mail.chungyo.net>
2021-08-27 15:27:04 +0800

    container

--- my_widgets/lib/screen/container.screen.dart
+++ my_widgets/lib/screen/container.screen.dart
@@ -1,16 +1,108 @@
 import 'package:flutter/material.dart';
 
-class ContainerScreen extends StatelessWidget {
+class ContainerScreen extends StatefulWidget {
+  @override
+  _ContainerScreen createState() => _ContainerScreen();
+}
+
+class _ContainerScreen extends State<ContainerScreen> {
+  var _index = 0;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Container")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+    var appBar = AppBar(
+      title: const Text("Container"),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.more_vert),
+          onPressed: () {
+            scaffoldKey.currentState?.openEndDrawer();
+          },
+        ),
+      ],
+    );
+
+    var drawer = Drawer(
+      child: ListView(
         children: [
-          Text("x"),
+          Container(
+            color: Colors.red,
+            height: 50,
+            child: Center(
+              child: Text(
+                "抽屜選單",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('EXIT'),
+            onTap: () => Navigator.pop(context),
+          ),
         ],
       ),
+    );
+
+    var body = Column(
+      children: [
+        Center(
+          child: Container(
+            constraints: BoxConstraints.tightFor(
+              width: 200.0,
+              height: 150.0,
+            ),
+            margin: const EdgeInsets.only(top: 20.0, bottom: 30.0),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color(0xFFFFE3D4),
+              border: Border.all(
+                color: Color(0xFFffd700),
+                width: 2,
+              ),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            transform: Matrix4.rotationZ(0.1),
+            child: Image.asset(
+              "assets/images/xFrame.jpg",
+            ),
+          ),
+        ),
+      ],
+    );
+
+    var bottomNavigationBar = BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: "首頁"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.add_a_photo_outlined), label: '相機'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.bug_report_outlined), label: "bug"),
+      ],
+      currentIndex: _index,
+      onTap: (index) {
+        setState(() {
+          _index = index;
+        });
+
+        if (index == 0) {
+          Navigator.pop(context);
+        }
+      },
+    );
+
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: appBar,
+      drawer: drawer,
+      endDrawer: drawer,
+      body: body,
+      bottomNavigationBar: bottomNavigationBar,
     );
   }
 }
